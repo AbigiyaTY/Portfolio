fd0e04921a7df87216b32ab92d5fb5566501459e
[Abigiya] <[abigiyatadesse23@gmail.com]>
2022-07-19 14:53:28 +0300

    Add ESlint linters to the project

--- js/script.js
+++ js/script.js
@@ -3,21 +3,19 @@ const openMenu = document.querySelector('.openMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const navitem = document.querySelectorAll('.mainMenu .nav-item .nav-link');
 
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
-
-// close menu when you click on a menu item 
-navitem.forEach(item => {
-    item.addEventListener('click', function() {
-        close();
-    })
-})
-
 function show() {
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
 }
 
 function close() {
-    mainMenu.style.top = "-100%";
+  mainMenu.style.top = '-100%';
 }
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+navitem.forEach((item) => {
+  item.addEventListener('click', () => {
+    close();
+  });
+});
