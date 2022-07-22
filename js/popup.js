cbfa65134214a9894d8bafbd1c42a7969b9d8f8f
[Abigiya] <[abigiyatadesse23@gmail.com]>
2022-07-22 20:57:53 +0300

    created to spearted javascript file and placed the script in there file

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,5 @@
-const mainMenu = document.querySelector('.mainMenu');
-const openMenu = document.querySelector('.openMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const navitem = document.querySelectorAll('.mainMenu .nav-item .nav-link');
 const overlay = document.querySelector('#overlay');
 const openModalButton = document.querySelectorAll('[data-modal-target]');
-
-function show() {
-  mainMenu.style.display = 'flex';
-  mainMenu.style.top = '0';
-}
-openMenu.addEventListener('click', show);
-
-function close() {
-  mainMenu.style.top = '-100%';
-}
-closeMenu.addEventListener('click', close);
-
-navitem.forEach((item) => {
-  item.addEventListener('click', () => {
-    close();
-  });
-});
 
 const mobileModalContent = [{
   title: 'Multi-post stories',
