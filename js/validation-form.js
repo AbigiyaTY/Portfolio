14f9de8d6a54a9395247e958d9b549b73d8f66cf
[Abigiya] <[abigiyatadesse23@gmail.com]>
2022-07-25 11:58:00 +0300

    Made validation for the email in the contact form

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('contact-form');
 const email = document.getElementById('email');
-const errorDisplay = document.getElementById('errorDisplay')
+const errorDisplay = document.getElementById('errorDisplay');
 const checker = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/g;
 
 form.addEventListener('submit', (e) => {
@@ -8,7 +8,7 @@ form.addEventListener('submit', (e) => {
         e.preventDefault();
         errorDisplay.style.visibility = 'visible';
         errorDisplay.classList.add('error-message');
-        errorDisplay.textContent = '*Your email address should be all in lowercase'
+        errorDisplay.textContent = '*Your email address should be all in lowercase';
     } else {
         errorDisplay.style.visibility = 'hidden';
     }
