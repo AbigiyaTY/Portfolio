e6ae11a6f1f895fed7228abbe289676f8b7eb90d
[Abigiya] <[abigiyatadesse23@gmail.com]>
2022-07-28 14:51:48 +0300

    Fixed linter error

--- js/email-validation.js
+++ js/email-validation.js
@@ -4,12 +4,12 @@ const errorDisplay = document.getElementById('errorDisplay');
 const checker = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/g;
 
 form.addEventListener('submit', (e) => {
-    if (!email.value.match(checker)) {
-        e.preventDefault();
-        errorDisplay.style.visibility = 'visible';
-        errorDisplay.classList.add('error-message');
-        errorDisplay.textContent = '*Your email address should be all be in lowercase';
-    } else {
-        errorDisplay.style.visibility = 'hidden';
-    }
+  if (!email.value.match(checker)) {
+    e.preventDefault();
+    errorDisplay.style.visibility = 'visible';
+    errorDisplay.classList.add('error-message');
+    errorDisplay.textContent = '*Your email address should be all be in lowercase';
+  } else {
+    errorDisplay.style.visibility = 'hidden';
+  }
 });
